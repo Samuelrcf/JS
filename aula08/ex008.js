b98f615f99bae40f2818401e8a13409589c9739d
Samuelrcf <rogenescarvalho@hotmail.com>
2022-04-02 17:57:16 -0300

    att

---
+++ aula08/ex008.js
@@ -0,0 +1,6 @@
+var vel = 121
+console.log(`The speed of your car is ${vel}Km/h`)
+if (vel > 120) {
+    console.log('You overpass the speed allowed. Fined.')
+}
+console.log('Drive always with your seat belt')
---
+++ aula08/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'United States of America'
+if (pais == 'Brazil') {
+    console.log('Brazilian!')
+} else{
+    console.log('Outsider!')
+}
